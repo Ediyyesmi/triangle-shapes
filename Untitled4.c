fe9a9f49d542ec844afd4bc1bfc4e8df2c50e43e
Ediyyesmi <143936492+Ediyyesmi@users.noreply.github.com>
2023-12-21 17:11:05 +0300

    Add files via upload

---
+++ Untitled4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int i, j, number;
+	
+	printf("enter a number: ");
+	scanf("%d" ,&number);
+	
+	for(i=1;i<=number;i++){
+		for(j=1;j<=number-i;j++){
+			printf(" ");
+		}
+		for(j=1;j<=i;j++){
+			printf("* ");
+		}
+		printf("\n");
+	}	
+	
+	
+	return 0;
+}
